b4946fd67b23221e1f5ea47f426a8a87525982da
rfletchr <>
2026-03-04 15:02:41 +0000

    feat: implement nuke app adapter

--- packages/notgun-nuke/startup/init.py
+++ packages/notgun-nuke/startup/init.py
@@ -1,4 +1,5 @@
 import notgun.bootstrap
+import notgun_nuke.adapters
 
 pipeline = notgun.bootstrap.init_from_env()
-print("loaded project", pipeline)
+pipeline.set_app(notgun_nuke.adapters.NukeApplicationAdapter())
