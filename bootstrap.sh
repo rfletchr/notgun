a65668a32d89483553eaf02a700076e5dc714c04
rfletchr <>
2026-03-04 14:42:34 +0000

    fix: remove release flags from bootstrapping script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,9 @@
-rez bind python -r --exe $(which python3)
-rez pip -r --install qtpy
-rez pip -r --install qtawesome
-rez pip -r --install PySide6
-rez pip -r --install requests
-rez pip -r --install git+https://github.com/shotgunsoftware/python-api.git
+# rez bind python -r --exe $(which python3)
+rez pip --install qtpy
+rez pip --install qtawesome
+rez pip --install PySide6
+rez pip --install requests
+rez pip --install git+https://github.com/shotgunsoftware/python-api.git
 (cd packages/notgun && rez build -i)
 (cd packages/notgun-launcher && rez build -i)
 (cd packages/notgun-nuke && rez build -i)
