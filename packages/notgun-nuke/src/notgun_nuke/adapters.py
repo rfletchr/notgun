3884cb34d53d1a1f6c5cbd4734c3b0c113822a67
rfletchr <>
2026-03-04 15:01:38 +0000

    fix: rename is_dirty to modified

--- packages/notgun-nuke/src/notgun_nuke/adapters.py
+++ packages/notgun-nuke/src/notgun_nuke/adapters.py
@@ -1,1 +1,19 @@
+import nuke
 import notgun.adapters
+
+
+class NukeApplicationAdapter(notgun.adapters.ApplicationAdapter):
+    def filepath(self) -> str | None:
+        return nuke.scriptName()
+
+    def modified(self) -> bool:
+        return nuke.root().modified()
+
+    def save(self):
+        if not nuke.scriptName():
+            raise notgun.adapters.NotSavedError("cannot save an un-named file")
+
+        nuke.scriptSave()
+
+    def save_as(self, filepath: str):
+        nuke.scriptSaveAs(filepath)
